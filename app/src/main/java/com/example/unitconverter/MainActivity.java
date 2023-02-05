c66cc2cd2aef2556d1dc337b2195b183110b14dd
Rohan Kumatkar <rk9326138@gmail.com>
2023-02-04 16:08:30 -0800

    This is my Unit conversion project.

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+        editText = findViewById(R.id.number);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                float gm = kg * 1000;
+                textView.setText("The value in grams is :"+gm);
+
+
+
+
+            }
+        });
+    }
+}
